2d70341d7660fbf6b2df8b8b782e13fb0a51f5a9
Leo <ignatevlev1@gmail.com>
2025-09-12 20:03:22 +0200

    Added a baseline CNN classifier model!

--- lab_notes.md
+++ lab_notes.md
@@ -9,3 +9,6 @@ Decided to use the galaxy zoo 2 dataset. With the help of AI (results that this 
 A snippet was saved to data/labels. The manifest with labels consists of 92 columns representing features of every image, as well as the scores in each category (spiral, smooth, merger, etc.)
 The last column corresponds to the final derived label, with a threshold of certainty of 0.6 (if not met - the galaxy is classified as ambiguous).
 
+## 2025-09-12
+Over the course of the last week built a baseline classifier, as well as a data pipeline for preprocessing (train_classifier.py and data_loader.py respectively).
+The model used is CNN (convolutional neural network) with a ResNet50 pretrained base (the starting configuration of layers is rather arbitrary, optimal parameters will be introduced later).
---
+++ src/train_classifier.py
@@ -0,0 +1,67 @@
+# imports
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import tensorflow.keras.layers as layers
+from data_loader import load_data
+
+labels_and_paths_csv_fp = r"C:\Users\79263\galaxy_morphology_ml_captioning\data\labels\labels_manifest_1000.csv" #labels filepath
+
+np.random.seed(5629) # random seed for reproducibility
+tf.random.set_seed(5629)
+
+baseline_CNN_model = tf.keras.models.Sequential([ # baseline CNN model (input -> resnet50 -> convolution and max pooling layers -> dense layers with relu and softmax
+    layers.InputLayer(input_shape=(224,224,3)),
+
+    #tf.keras.applications.ResNet50(include_top=False,),
+
+    layers.Conv2D(filters=64, kernel_size=3, strides=1, activation='relu'),
+    layers.MaxPool2D(pool_size=3, strides=3),
+
+    layers.Conv2D(filters=128, kernel_size=3, strides=1, activation='relu'),
+    layers.MaxPool2D(pool_size=3, strides=3),
+
+    layers.Conv2D(filters=256, kernel_size=3, strides=1, activation='relu'),
+    layers.Conv2D(filters=256, kernel_size=3, strides=1, activation='relu'),
+    layers.MaxPool2D(pool_size=3, strides=3),
+
+    layers.Flatten(),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(4, activation='softmax')
+])
+
+optimizer = tf.keras.optimizers.Adam(epsilon=0.001) # Adam optimizer as a superior for this task
+
+baseline_CNN_model.compile( # compilation
+    optimizer=optimizer,
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+train_sample, valid_sample = load_data(labels_and_paths_csv_fp) # data loading (see data_loader.py)
+
+history = baseline_CNN_model.fit( # fitting
+    train_sample,
+    validation_data=valid_sample,
+    epochs=50
+)
+
+hist = history.history
+
+# plotting time!
+plt.plot(hist['loss'], label='Train Loss')
+plt.plot(hist['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+plt.show()
+
+plt.plot(hist['accuracy'], label='Train Accuracy')
+plt.plot(hist['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.legend()
+plt.show()
