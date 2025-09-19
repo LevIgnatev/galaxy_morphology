8e46a0e27eb5216aea10ead0e0eb358b6ec8b50f
Lev Ignatev <ignatevlev1@gmail.com>
2025-09-19 21:37:24 +0300

    Finished the 1st, data exploration notebook

--- src/csv_changer.py
+++ src/csv_changer.py
@@ -1,8 +1,8 @@
 import pandas as pd
 fn = r"C:\Users\user\PycharmProjects\galaxy_morphology_ml_captioning\data\processed\manifest_with_labels.csv"
 df = pd.read_csv(fn)
-df["filepath_PC"] = df["filepath"]
-df["filepath_laptop"] = df["filepath"].str.replace(r"user\PycharmProjects\galaxy_morphology_ml_captioning", r"79263\galaxy_morphology_ml_captioning", regex=False)
+#df["filepath_PC"] = df["filepath"]
+df.drop('filepath', axis=1, inplace=True)
 out = fn
 df.to_csv(out, index=False)
 print("Wrote", out)
