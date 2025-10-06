7d38d68134d5f97cdb94dc94e072aa0bc9330e62
Lev Ignatev <ignatevlev1@gmail.com>
2025-10-06 18:50:32 +0300

    3rd notebook on baselines added.

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -4,42 +4,45 @@ import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow.keras.layers as layers
+from keras.src.applications.convnext import preprocess_input
+from keras.src.callbacks import EarlyStopping
+from prompt_toolkit.key_binding.bindings.named_commands import self_insert
+
 from data_loader import load_data
 
-labels_and_paths_csv_fp_PC_sample = r"C:\Users\user\PycharmProjects\galaxy_morphology_ml_captioning\data\labels\labels_manifest_1000.csv" #labels filepath (PC)
-labels_and_paths_csv_fp_laptop_sample = r"C:\Users\79263\galaxy_morphology_ml_captioning\data\labels\labels_manifest_1000.csv" #labels filepath (laptop)
-labels_and_paths_csv_fp_PC_full = r"C:\Users\user\PycharmProjects\galaxy_morphology_ml_captioning\data\processed\manifest_train_and_val.csv"
-labels_and_paths_csv_fp_laptop_full = r"C:\Users\79263\galaxy_morphology_ml_captioning\data\processed\manifest_train_and_val.csv"
+labels_and_paths_csv_fp_PC_sample = r"C:\Users\user\PycharmProjects\galaxy_morphology_ml_captioning\data\labels\labels_manifest_1000.csv" # sample labels filepath (PC)
+labels_and_paths_csv_fp_laptop_sample = r"C:\Users\79263\galaxy_morphology_ml_captioning\data\labels\labels_manifest_1000.csv" # sample labels filepath (laptop)
+labels_and_paths_csv_fp_PC_full = r"C:\Users\user\PycharmProjects\galaxy_morphology_ml_captioning\data\processed\manifest_train_and_val.csv" # full labels filepath (PC)
+labels_and_paths_csv_fp_laptop_full = r"C:\Users\79263\galaxy_morphology_ml_captioning\data\processed\manifest_train_and_val.csv" # full labels filepath (laptop)
+full_dataset_present = False
 
 np.random.seed(5629) # random seed for reproducibility
 tf.random.set_seed(5629)
 
+
 baseline_CNN_model = tf.keras.models.Sequential([ # baseline CNN model (input -> resnet50 -> convolution and max pooling layers -> dense layers with relu and softmax
     layers.InputLayer(input_shape=(224,224,3)),
 
-    tf.keras.applications.ResNet50(include_top=False,),
+    layers.RandomFlip(["horizontal", "vertical"]),
+    layers.RandomRotation(0.2),
+    layers.RandomTranslation(0.1, 0.1),
 
-    layers.Conv2D(filters=64, kernel_size=3, strides=1, activation='relu', padding='same'),
-    layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+    layers.Lambda(tf.keras.applications.resnet50.preprocess_input, name="resnet_preproc"),
 
-    layers.Conv2D(filters=128, kernel_size=3, strides=1, activation='relu', padding='same'),
-    layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+    tf.keras.applications.ResNet50(include_top=False, weights='imagenet', pooling='avg'),
 
-    layers.Conv2D(filters=256, kernel_size=3, strides=1, activation='relu', padding='same'),
-    layers.Conv2D(filters=256, kernel_size=3, strides=1, activation='relu', padding='same'),
-    layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
-
-    layers.Flatten(),
-    layers.Dense(32, activation='relu'),
-    layers.Dense(4, activation='softmax')
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(5 if full_dataset_present else 4, activation='softmax')
 ])
+baseline_CNN_model.layers[5].trainable = False
 
-optimizer = tf.keras.optimizers.Adam(epsilon=0.001) # Adam optimizer as a superior for this task
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4) # Adam optimizer as a superior for this task
 
 baseline_CNN_model.compile( # compilation
     optimizer=optimizer,
     loss='categorical_crossentropy',
-    metrics=['accuracy']
+    metrics=['accuracy'],
 )
 
 train_sample, valid_sample = load_data(labels_and_paths_csv_fp_PC_sample, "PC") # data loading (see data_loader.py)
@@ -47,7 +50,8 @@ train_sample, valid_sample = load_data(labels_and_paths_csv_fp_PC_sample, "PC") 
 history = baseline_CNN_model.fit( # fitting
     train_sample,
     validation_data=valid_sample,
-    epochs=1
+    epochs=5,
+    callbacks=[EarlyStopping(patience=5, restore_best_weights=True)],
 )
 
 hist = history.history
