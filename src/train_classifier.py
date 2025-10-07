507d3799ea56fbf182319deb4b3d8f04b49150be
Lev Ignatev <ignatevlev1@gmail.com>
2025-10-07 10:53:49 +0300

    Improved baseline classifier (again)

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -19,23 +19,21 @@ full_dataset_present = False
 np.random.seed(5629) # random seed for reproducibility
 tf.random.set_seed(5629)
 
+inputs = tf.keras.Input((224,224,3))
+x = layers.RandomFlip(["horizontal", "vertical"])(inputs)
+x = layers.RandomRotation(0.2)(x)
+x = layers.RandomTranslation(0.1, 0.1)(x)
+x = tf.keras.applications.resnet50.preprocess_input(x)
 
-baseline_CNN_model = tf.keras.models.Sequential([ # baseline CNN model (input -> resnet50 -> convolution and max pooling layers -> dense layers with relu and softmax
-    layers.InputLayer(input_shape=(224,224,3)),
-
-    layers.RandomFlip(["horizontal", "vertical"]),
-    layers.RandomRotation(0.2),
-    layers.RandomTranslation(0.1, 0.1),
-
-    layers.Lambda(tf.keras.applications.resnet50.preprocess_input, name="resnet_preproc"),
-
-    tf.keras.applications.ResNet50(include_top=False, weights='imagenet', pooling='avg'),
-
-    layers.Dense(128, activation='relu'),
-    layers.Dropout(0.2),
-    layers.Dense(5 if full_dataset_present else 4, activation='softmax')
-])
-baseline_CNN_model.layers[5].trainable = False
+resnet = tf.keras.applications.ResNet50(
+    include_top=False, weights="imagenet", pooling="avg"
+)
+resnet.trainable = False
+x = resnet(x, training=False)
+x = layers.Dense(128, activation="relu")(x)
+x = layers.Dropout(0.2)(x)
+outputs = layers.Dense(5 if full_dataset_present else 4, activation="softmax")(x)
+baseline_CNN_model = tf.keras.Model(inputs, outputs)
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4) # Adam optimizer as a superior for this task
 
@@ -50,8 +48,8 @@ train_sample, valid_sample = load_data(labels_and_paths_csv_fp_PC_sample, "PC") 
 history = baseline_CNN_model.fit( # fitting
     train_sample,
     validation_data=valid_sample,
-    epochs=5,
-    callbacks=[EarlyStopping(patience=5, restore_best_weights=True)],
+    epochs=30,
+    callbacks=[EarlyStopping(patience=10, restore_best_weights=True)],
 )
 
 hist = history.history
