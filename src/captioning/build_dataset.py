3729641adf8fe0be163915b5d922ebed37109e55
Leo <ignatevlev1@gmail.com>
2025-11-04 13:39:52 +0300

    Captioner model foundations

--- src/captioning/build_dataset.py
+++ src/captioning/build_dataset.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
+import json
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATA_PATH = PROJECT_ROOT / "data" / "labels"
@@ -13,9 +13,12 @@ labels_fp = DATA_PATH / "labels_manifest_1000.csv"
 laptop_or_PC = "laptop"
 manifest_fp = ""
 
+config_fp = DATA_PATH / "captions" / "config.json"
+config = json.load(open(config_fp))
+
 from tokenizer import encode
 
-def clean_ds_ffs():
+def dataset():
     manifest = pd.read_csv(manifest_fp)
     train_ids = pd.read_csv(
         caption_train_path,
@@ -47,7 +50,16 @@ def clean_ds_ffs():
         image.set_shape([None, None, 3])
         image = tf.image.resize(image, [224, 224])
         image = tf.cast(image, tf.float32)
+        image.set_shape((224, 224, 3))
+
         cap_in, cap_out, mask = encode(caption)
+        cap_in = tf.cast(cap_in, tf.int32)
+        cap_out = tf.cast(cap_out, tf.int32)
+        mask = tf.cast(mask, tf.float32)
+        tf.ensure_shape(cap_in, (config['max_len'],))
+        tf.ensure_shape(cap_out, (config['max_len'],))
+        tf.ensure_shape(mask, (config['max_len'],))
+
         return image, cap_in, cap_out, mask
 
     AUTOTUNE = tf.data.AUTOTUNE
@@ -57,6 +69,7 @@ def clean_ds_ffs():
     train_ds = (
         train_ds.shuffle(buffer_size=len(train_paths_list), seed=37)
         .map(PREPROCESS, num_parallel_calls=AUTOTUNE)
+        .map(lambda image, cap_in, cap_out, mask: ((image, cap_in), cap_out, mask))
         .batch(32)
         .prefetch(AUTOTUNE)
     )
@@ -66,91 +79,7 @@ def clean_ds_ffs():
     valid_ds = (
         valid_ds
         .map(PREPROCESS, num_parallel_calls=AUTOTUNE)
-        .batch(32)
-        .prefetch(AUTOTUNE)
-    )
-
-    return train_ds, valid_ds
-
-def dataset(): # main function for loading the data from filepaths
-    df1 = pd.read_csv(all_captions_path, dtype={"objid": str})
-    train_ids = pd.read_csv(
-        caption_train_path,
-        header=None,
-        names=["objid"],
-        dtype=str,
-        sep=r"\s+",
-        engine="python",
-    )
-    valid_ids = pd.read_csv(
-        caption_valid_path,
-        header=None,
-        names=["objid"],
-        dtype=str,
-        sep=r"\s+",
-        engine="python",
-    )
-    df_train = df1.merge(train_ids, how="inner", on="objid").reset_index(drop=True)
-    df_valid = df1.merge(valid_ids, how="inner", on="objid").reset_index(drop=True)
-
-    captions_train = df_train["caption"].astype(str).tolist()
-    captions_valid = df_valid["caption"].astype(str).tolist()
-
-    captions_train_list = []
-    for caption in captions_train:
-        cap_in, cap_out, mask = encode(caption)
-        captions_train_list.append([cap_in, cap_out, mask])
-
-    captions_valid_list = []
-    for caption in captions_valid:
-        cap_in, cap_out, mask = encode(caption)
-        captions_valid_list.append([cap_in, cap_out, mask])
-
-    df = pd.read_csv(labels_fp) # read the labels csv file
-    labels_and_fp_cols = df[["derived_label", f"filepath_{laptop_or_PC}"]] # select needed labels
-
-    train_df, valid_df = train_test_split(labels_and_fp_cols, # split the data for training (80%) and validation (20%)
-                                          test_size=0.2,
-                                          stratify=labels_and_fp_cols["derived_label"],
-                                          random_state=37)
-
-    labels_to_indexes = {} # create a dictionary with labels and indexes (elliptical=0, spiral=1, etc.)
-    unique_labels = labels_and_fp_cols["derived_label"].nunique()
-    for i in range (unique_labels):
-        labels_to_indexes[labels_and_fp_cols["derived_label"].unique()[i]] = i
-
-    train_paths_list = train_df[f"filepath_{laptop_or_PC}"].tolist() # convert dataframes to lists
-    valid_paths_list = valid_df[f"filepath_{laptop_or_PC}"].tolist()
-    train_labels_list = []
-    valid_labels_list = []
-    for i in train_df["derived_label"]:
-        train_labels_list.append(labels_to_indexes[i])
-    for i in valid_df["derived_label"]:
-        valid_labels_list.append(labels_to_indexes[i])
-
-    def PREPROCESS(path, label): # function to flatten the images and one-hot encode the labels
-        image = tf.io.read_file(path)
-        image = tf.image.decode_image(image, channels=3)
-        image.set_shape([None, None, 3])
-        image = tf.image.resize(image, [224, 224])
-        image = tf.cast(image, tf.float32) # cast the image to a tensor with numbers between 0 and 255
-        labels = tf.one_hot(label, unique_labels) # one-hot encoding the labels
-        return image, labels
-
-    AUTOTUNE = tf.data.AUTOTUNE # AUTOTUNE variable for an optimized performance
-
-    train_ds = tf.data.Dataset.from_tensor_slices((train_paths_list, captions_train_list)) # main data pipeline (map, then the classic shuffle - batch - prefetch) for training data
-    train_ds = (
-        train_ds.shuffle(buffer_size=len(train_paths_list), seed=37)
-        .map(PREPROCESS, num_parallel_calls=AUTOTUNE)
-        .batch(32)
-        .prefetch(AUTOTUNE)
-    )
-
-    valid_ds = tf.data.Dataset.from_tensor_slices((valid_paths_list, captions_valid_list)) # main data pipeline (map, then the shuffle - batch - prefetch) for validation data
-    valid_ds = (
-        valid_ds
-        .map(PREPROCESS, num_parallel_calls=AUTOTUNE)
+        .map(lambda image, cap_in, cap_out, mask: ((image, cap_in), cap_out, mask))
         .batch(32)
         .prefetch(AUTOTUNE)
     )
---
+++ src/captioning/model_tf.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import tensorflow.keras.layers as layers
+import json
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+DATA_PATH = PROJECT_ROOT / "data" / "labels"
+manifest_fp = ""
+config_fp = DATA_PATH / "captions" / "config.json"
+vocab_fp = DATA_PATH / "captions" / "vocab.json"
+
+from build_dataset import dataset
+
+np.random.seed(5629) # random seed for reproducibility
+tf.random.set_seed(5629)
+
+config = json.load(open(config_fp))
+vocab = json.load(open(vocab_fp))
+vocab_size = len(vocab)
+
+# Encoder
+inputs = tf.keras.Input((224,224,3))
+
+x = layers.RandomFlip(["horizontal", "vertical"])(inputs)
+x = layers.RandomRotation(0.2)(x)
+x = layers.RandomTranslation(0.1, 0.1)(x)
+x = tf.keras.applications.resnet50.preprocess_input(x)
+
+resnet = tf.keras.applications.ResNet50(
+    include_top=False, weights="imagenet", pooling="avg"
+)
+resnet.trainable = False
+x = resnet(x, training=False)
+img_emb = layers.Dense(256, activation="relu")(x)
+
+h0 = layers.Dense(units=256, activation='tanh')(img_emb)
+c0 = layers.Dense(units=256, activation='tanh')(img_emb)
+
+# Decoder
+token_input = layers.Input((config['max_len'],), dtype='int32')
+
+token_embedding = layers.Embedding(vocab_size, 256, mask_zero=True)(token_input)
+LSTM_out = layers.LSTM(256, return_sequences=True)(token_embedding, initial_state=[h0, c0])
+dropped = layers.Dropout(0.3)(LSTM_out)
+logits = layers.TimeDistributed(layers.Dense(vocab_size))(dropped)
+
+
+captioner_model = tf.keras.Model(inputs=[inputs, token_input], outputs=logits)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+
+captioner_model.compile(
+    optimizer=optimizer,
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+)
+
+train_ds, valid_ds = dataset()
+
+history = captioner_model.fit(
+    train_ds,
+    validation_data=valid_ds,
+    epochs=2,
+    #callbacks=[EarlyStopping(patience=10, restore_best_weights=True)],
+)
