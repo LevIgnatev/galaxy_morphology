195e5ff98bd2eab7799f955cb753317f0b115401
Lev Ignatev <ignatevlev1@gmail.com>
2025-11-08 11:41:05 +0300

    Minor improvements

--- src/scripts/csv_changer.py
+++ src/scripts/csv_changer.py
@@ -4,15 +4,15 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATA_PATH = PROJECT_ROOT / "data" / "labels"
 
-fn = DATA_PATH / "labels_manifest_1000.csv"
+fn = PROJECT_ROOT / "data" / "processed" / "manifest_with_labels.csv"
 
 
-out = DATA_PATH / "labels_manifest_1000_new.csv"
+out = PROJECT_ROOT / "data" / "processed" / "manifest_with_labels.csv"
 df = pd.read_csv(fn)
 
-#df["filepath"] = [i.replace("[redacted]", "") for i in df["filepath_PC"]]
-#df.drop("filepath_PC", axis=1, inplace=True)
-#df.drop("filepath_laptop", axis=1, inplace=True)
+df["filepath"] = [str(i).replace("[redacted]", "") for i in df["filepath_PC"]]
+df.drop("filepath_PC", axis=1, inplace=True)
+df.drop("filepath_laptop", axis=1, inplace=True)
 
 df.to_csv(out, index=False)
 print("Donezo!", out)
