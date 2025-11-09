11b2891cfdeb32e312a34b69077e20911fdeaa40
LevIgnatev <ignatevlev1@gmail.com>
2025-11-09 20:52:13 +0300

    -

--- src/classifying/train_classifier.py
+++ src/classifying/train_classifier.py
@@ -49,14 +49,14 @@ train_sample, valid_sample = load_data(labels_and_paths_csv_fp_full) # data load
 history = baseline_CNN_model.fit( # fitting
     train_sample,
     validation_data=valid_sample,
-    epochs=1,
+    epochs=50,
     #callbacks=[EarlyStopping(patience=10, restore_best_weights=True)],
 )
 
 SAVE_DIR = Path(os.getenv("OUT_DIR", Path.cwd() / "outputs"))
 SAVE_DIR.mkdir(parents=True, exist_ok=True)
 
-baseline_CNN_model.save_weights(SAVE_DIR / "ckpt_classifier_full_small.h5")
+baseline_CNN_model.save_weights(SAVE_DIR / "ckpt_classifier_full.h5")
 
 hist = history.history
 
